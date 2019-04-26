d0bcd25ca4fa46970d5496021b6c28fab917450b
flawebadm <46044003+flawebadm@users.noreply.github.com>
2019-04-26 12:37:22 +0200

    Update guestbook.js

--- docs/guestbook.js
+++ docs/guestbook.js
@@ -1,7 +1,8 @@
 /**
  * Web application
  */
-const apiUrl = 'https://service.us.apiconnect.ibmcloud.com/gws/apigateway/api/CHANGEME/guestbook';
+// ##ff## const apiUrl = 'https://service.us.apiconnect.ibmcloud.com/gws/apigateway/api/CHANGEME/guestbook';
+const apiUrl = 'https://ef1340da.eu-gb.apiconnect.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
